5e509b8e187b877957630b2fb9b51638a36deed9
sbrenner132 <samuelbrenner@brandeis.edu>
2021-11-11 22:50:33 -0500

    cpa4

---
+++ README.md
@@ -0,0 +1,50 @@
+# Clothes Management
+
+## Elevator Pitch
+
+Have you ever wanted to create a custom style for yourself without wasting too much brainpower? Clothes Management is an app that allows you to upload photos of clothing items, categorize them, and create random outfits for you to use whenever you need! With the latest updates, you can also share custom outfits with friends via email.
+
+## Screenshots 
+
+<img src="img1.jpg" alt="a" width="200"/>
+<img src="img2.jpg" alt="a" width="200"/>
+<img src="img3.jpg" alt="a" width="200"/>
+<img src="img4.jpg" alt="a" width="200"/>
+<img src="img5.jpg" alt="a" width="200"/>
+<img src="img6.jpg" alt="a" width="200"/>
+
+## Technologies Used
+
+Some technolgies used to make this application include:
+
+- React 
+- React Native
+- Expo
+- React Navigation
+- qs (Query Management)
+- Expo Camera
+- Expo Image Picker
+
+This application also uses modern JS techniques, including `useState` and `useEffect` instead of older frameworks that rely on component mounting and unmounting and `this.state`.
+
+## Instructions to Run Locally
+
+To run this app locally, once cloned and redirected into the appropriate folder, please follow these steps:
+
+1. To run on your phone
+
+    ```console
+    npm install
+    expo start
+    ```
+
+    From there, expo will open a window in your browser that will allows you to collect to a set of different devices. To run on your phone, please set the connection to `Tunnel` and scan the QR code with your Expo app. 
+
+2. To run in a web browser
+
+    ```terminal
+    npm install
+    yarn web
+    ```
+
+    This should open a window in your browser that, once the code is compiled, will open another window in your browser that contains a web version of the code.
--- components/Outfit.js
+++ components/Outfit.js
@@ -1,7 +1,21 @@
-import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import React, { useState } from "react";
+import { Alert, StyleSheet, Text, View, Image, Button, Modal, Pressable, TextInput } from "react-native";
+import sendEmail from "../scripts/email";
 
 export default ({ outfit }) => {
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [to, setTo] = useState("");
+
+  const sendData = () => {
+    const top = outfit.top ? outfit.top.name : "No outfit top";
+    const bottom = outfit.bottom ? outfit.bottom.name : "no outfit bottom";
+    const headwear = outfit.headwear ? outfit.headwear.name : "no outfit headwear";
+    sendEmail(to, 'An outfit has been shared with you!', `Here's the latest fashion trend!\n${top}, ${bottom}, ${headwear}`, {
+      cc: 'samuelbrenner@brandeis.edu'
+    }).then(() => Alert.alert("Sent!"));
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.item}>
@@ -40,6 +54,44 @@ export default ({ outfit }) => {
           </View>
         ) : null}
       </View>
+      <View style={styles.email_button}>
+        <Button title="Share via Email" onPress={() => setModalVisible(true)} />
+      </View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Enter an email to share with</Text>
+            <TextInput
+              value={to}
+              placeholder={"example@gmail.com"}
+              style={styles.text_input}
+              onChangeText={(text) => setTo(text)}
+            />
+            <View style={{ paddingBottom: 10, paddingTop: 10 }}>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => { sendData(); setModalVisible(!modalVisible) }}
+              >
+                <Text style={styles.textStyle}>Send</Text>
+              </Pressable>
+            </View>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Cancel</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -54,5 +106,50 @@ const styles = StyleSheet.create({
   item: {
     flexDirection: "column",
     flex: 3,
+  },
+  email_button: {
+    flex: 2,
+    justifyContent: "center"
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
   },
 });
